5ac93fe07b1ec2dae22d3eb4b5b0a0d88b534b54
Goutham Kumar <47983326+gkdevops@users.noreply.github.com>
2025-11-08 19:26:38 +0530

    Enhance module_demo.py to import and use greetings

--- 07-Python-Modules/module_demo.py
+++ 07-Python-Modules/module_demo.py
@@ -1,4 +1,14 @@
 import greetings # Use the module
+import sys
+import json
 
-message = greetings.say_hello("Alice")
-print(message) # Output: Hello, Alice!
+# To load modules from different locations
+sys.path.append('/C:/Desktop/python-data-engineer-main')
+
+message_hello = greetings.say_hello("Alice")
+
+message_goodbye = greetings.say_goodbye("Bob")
+print(message_hello)
+print(message_goodbye)
+
+#print(message) # Output: Hello, Alice!
